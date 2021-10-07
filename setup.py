67b8d1988e5a27924a4c34eb662bfa39a294acad
Rogério Silva <eng.rogerio@gmail.com>
2021-10-07 09:15:51 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env pyhthon3
-import sys
-sys.path.insert(0, 'ggv2utils')
-from ggv2utils import ggmq
 currentVersion = "0.0.1"
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'ggv2utils',
-    packages=['ggv2utils'],
+    packages=find_packages(),
     version = currentVersion,
     description = 'Message queue services for greengrass v2 components.',
     author = 'Guardian Industries',
@@ -16,15 +11,8 @@ setup(
     url = 'https://github.com/engrogerio/greengrassv2utils.git',
     download_url = 'https://github.com/engrogerio/greengrassv2utils/archive/refs/heads/main.zip',
     keywords = ['aws', 'iot', 'mqtt', 'ipc'],
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+    classifiers = []
+    install_requires=[
+        'awsiotsdk==1.7.1',
     ]
 )
