03a9b33a910cc8565640c477f2458dad2c750a9f
Rogério Silva <eng.rogerio@gmail.com>
2021-10-07 08:58:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     description = 'Message queue services for greengrass v2 components.',
     author = 'Guardian Industries',
     author_email = 'rsilva@guardian.com',
-    url = 'https://kochsource.io/rsilva/greengrassv2utils.git',
-    download_url = 'https://kochsource.io/rsilva/greengrassv2utils/-/archive/main/greengrassv2utils-main.zip',
+    url = 'https://github.com/engrogerio/greengrassv2utils.git',
+    download_url = 'https://github.com/engrogerio/greengrassv2utils/archive/refs/heads/main.zip',
     keywords = ['aws', 'iot', 'mqtt', 'ipc'],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
